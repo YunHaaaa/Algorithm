7c5280d3ccf5c45deebfce4b4bb5353214f53df7
HaEun <63325450+YunHaaaa@users.noreply.github.com>
2022-01-10 00:18:42 +0900

    Create 2810_컵홀더.cpp

---
+++ 2810_컵홀더.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <stack>
+#include <algorithm>
+using namespace std;
+
+int main(void) {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	int num;
+	string s;
+	cin >> num;
+	cin >> s;
+	
+	int count = 1;
+	int L = 0;
+	for (int i = 0; i < s.length(); i++) {
+		if (s[i] == 'S')
+			count++;
+		else {
+			L++;
+			if (L == 2) {
+				count++;
+				L = 0;
+			}
+		}
+	}
+	if (L == 1)
+		count++;
+	cout << min(count, num) << "\n";
+	return 0;
+}
