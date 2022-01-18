47dd0f1879eec297479a474050345d93350733ee
HaEun <63325450+YunHaaaa@users.noreply.github.com>
2022-01-19 00:57:42 +0900

    Update 2089-2진수.cpp

--- 2089-2진수.cpp
+++ 2089-2진수.cpp
@@ -2,21 +2,41 @@
 #include <cmath>
 using namespace std;
 
-int main() {
-	int N, bin = 0;
-	cin >> N;
-	
-	if (N < 0) {
-		while (N != 0) {
-			N /= 2;
-			bin++;
-		}
-		N += pow(2, bin);
+int binary(int dec) {
+	int count=0;
+	while (dec != 0) {
+		dec /= 2;
+		count++;
 	}
-	
-	while (N != 0) {
+	return count;
+}
 
+int main() {
+	int N, bin = 0, len = 0;
+	cin >> N;
+
+	if (N < 0) {
+		len = binary(N) + 2;
 	}
+	else
+		len = binary(N);
+
+	int* a = new int[len];
+	for (int i = 0; i < len; i++)
+		a[i] = 0;
+
+	int mbin = 0;
+	while (N != 0) { 
+		mbin = binary(N);
+		if ( mbin % 2 ) { // == 1 == -
+			N += pow(2, mbin+1);
+		}
+		else { // == 0 == +
+			a[mbin] = 1;
+		}
+	}
+	for (int i = 0; i < N; i++)
+		cout << a[i];
 
 	return 0;
 }
