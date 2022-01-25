9aa4e7567ac73cb1b3f5b2f85773263a913bc7a1
HaEun <63325450+YunHaaaa@users.noreply.github.com>
2022-01-25 23:52:26 +0900

    Create 5568_카드놓기.cpp

---
+++ 5568_카드놓기.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+/*
+팩토리얼 함수
+  if 중복
+    return 팩토리얼 함수 / 중복개수 팩토리얼
+  return 팩토리얼 함수
+*/
+
+int main() {
+   
+  int n, k;
+  cin >> n >> k;
+  
+  int *a = new a[n];
+  
+  for (int i=0; i<n; i++) {
+    cin >> a[i];
+  }
+  
+  cout << 팩토리얼함수(n, k, a);
+  
+  return 0;
+}
