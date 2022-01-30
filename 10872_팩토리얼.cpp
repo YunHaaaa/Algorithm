646660abed516a065cce66330581f6d43abad650
HaEun <63325450+YunHaaaa@users.noreply.github.com>
2022-01-30 13:55:26 +0900

    Update 10872_팩토리얼.cpp

--- 10872_팩토리얼.cpp
+++ 10872_팩토리얼.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 using namespace std;
 
+int factorial(int a) {
+	if (a > 1) {
+		return a * factorial(a - 1);
+	}
+	return 1;
+}
+
 int main() {
 
 	int N;
 	cin >> N;
 
+	cout << factorial(N);
+	
 
 }
