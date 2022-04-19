a952a883f38cd6e0466b20e9b9fb163b24b7773e
HaEun <63325450+YunHaaaa@users.noreply.github.com>
2022-04-19 21:44:38 +0900

    Update 1874_스택.cpp

--- 1874_스택.cpp
+++ 1874_스택.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<deque>
 using namespace std;
 
 int main() {
@@ -6,25 +7,38 @@ int main() {
     cin >> n;
     int* b = new int[n];
     int* stack = new int[n];
+    deque<char> que;
 
     for (int i = 0; i<n; i++)
         cin >> b[i];
-    int bar = 1, count = 0, index = -1, flag = 0; 
+    int bar = 1, count = 0, index = -1, flag = 1; 
+
     while (count < n) {
         for (bar; bar<=b[count]; bar++){
             stack[++index] = bar;
-            cout << "index : " << index << " bar : " << bar << endl; 
-            cout << "+" << endl;
-            flag++;
+            //cout << "index : " << index << " bar : " << bar << endl; 
+            que.push_back('+');
         }
-        cout << stack[index] << " " << b[count] << endl;
+        //cout << stack[index] << " " << b[count] << endl;
 
+        if (stack[index] != b[count]) {
+            cout << "NO" << endl;
+            flag = 0;
+            break;
+        }
+            
         while (stack[index] == b[count]){
-            cout << "index : " << index << " count : " << count-1 << endl; 
+            //cout << "index : " << index << " count : " << count-1 << endl; 
             index--;
             count++;
-            cout << "-" << endl;
-            flag--;
+            que.push_back('-');
+        }
+
+    }
+
+    if (flag) {
+        for (int i=0; i<que.size(); i++){
+            cout << que[i] << endl;
         }
     }
 }
